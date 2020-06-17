0b50aeac8b1b617c33814e1cf14fb05949440c58
Jenniffer <jenlpac@yahoo.com>
2020-06-16 22:17:07 -0500

    ensure length and character selections are valid

--- Develop/script.js
+++ Develop/script.js
@@ -12,21 +12,36 @@ var generatePassword = function() {
   var passwordLength = prompt("How long would you like your password to be? Select 8 to 128 characters.");
   // Convert to integer
   passwordLength = parseInt(passwordLength);
-
-  
   console.log (passwordLength);
-  var lowerCaseConfirm = confirm("Would you like lowercase characters in your password?");
-  console.log (lowerCaseConfirm);
-  var upperCaseConfirm = confirm("Would you like uppercase characters in your password?");
-  console.log (upperCaseConfirm);
-  var numConfirm = confirm("Would you like numbers in your password?");
-  console.log (numConfirm);
-  var specialConfirm = confirm("Would you like special characters in your password?");
-  console.log (specialConfirm);
-   
-    if (lowerCaseConfirm) {
 
+
+  // Ensure length selection is within defined terms
+  if(passwordLength >= 8 && passwordLength <= 128) {
+
+    // Create function to call if character type selections are not valid
+    var charType = function() {
+      var lowerCaseConfirm = confirm("Would you like lowercase characters in your password?");
+      console.log (lowerCaseConfirm);
+      var upperCaseConfirm = confirm("Would you like uppercase characters in your password?");
+      console.log (upperCaseConfirm);
+      var numConfirm = confirm("Would you like numbers in your password?");
+      console.log (numConfirm);
+      var specialConfirm = confirm("Would you like special characters in your password?");
+      console.log (specialConfirm);
+      
+      // Ensure at least one character type is selected
+      if(!lowerCaseConfirm && !upperCaseConfirm && !numConfirm && !specialConfirm) {
+        alert("Please select at least one character type.");
+        return charType();
+      }
     }
+  charType();
+  }
+  else {
+    alert("Please select a valid password length.");
+    return generatePassword();
+  }
+
 
 }
 
